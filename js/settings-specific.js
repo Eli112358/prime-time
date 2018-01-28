8296e9aa7c2974013a1fe64a56672b6778006e73
Eli112358 <Eli112358.SARP@hotmail.com>
2018-01-28 00:30:37 -0500

    Added callback function in insertCodeFromFile

--- js/settings-specific.js
+++ js/settings-specific.js
@@ -2,17 +2,19 @@ var settings;
 function initSettingsSpecific() {
   settings = initSettings({
     'store': ['range', 'formula', 'offset'],
-    'toggle': ['mode']
-  });
-  settings.toggle.setup({
-    'ele': settings.toggle.ele.mode,
-    'property': 'value',
-    'stored': true,
-    'values': ['Game', 'Tool'],
+    'toggle': ['mode'],
     'func': () => {
-      ['mode-main', 'settings-section'].forEach((n) => {
-        [].forEach.call(getByClass(n), toggle)
-      })
+      settings.toggle.setup({
+        'ele': settings.toggle.ele.mode,
+        'property': 'value',
+        'stored': true,
+        'values': ['Game', 'Tool'],
+        'func': () => {
+          ['mode-main', 'settings-section'].forEach((n) => {
+            [].forEach.call(getByClass(n), toggle)
+          })
+        }
+      });
     }
   });
 }
