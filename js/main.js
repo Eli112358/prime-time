ad62474e1284663f786b0092ae4ae4df97852d1c
Eli112358 <Eli112358.SARP@hotmail.com>
2018-01-28 01:10:23 -0500

    Typo fix

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function load() {
       ele.classList.add('btn')
     })
   });
-  insertCodeFromFile('path': 'https://eli112358.github.io/snippets/header.txt', 'element': document.getElementById('downloads'), 'func': () => {
-    insertCodeFromFile('path': 'https://eli112358.github.io/snippets/donate.txt', 'element': document.getElementById('donate'));
-  });
+  insertCodeFromFile({'path': 'https://eli112358.github.io/snippets/header.txt', 'element': document.getElementById('downloads'), 'func': () => {
+    insertCodeFromFile({'path': 'https://eli112358.github.io/snippets/donate.txt', 'element': document.getElementById('donate')});
+  }});
 }
