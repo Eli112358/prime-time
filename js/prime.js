029404c6198db1850eec73ffa75f98358481c5cf
Eli112358 <Eli112358D@gmail.com>
2020-03-26 15:30:17 -0400

    Simplify and optimize algorithm

--- js/prime.js
+++ js/prime.js
@@ -17,28 +17,30 @@ function initPrime() {
 	prime.isFactor = (a,b) => a%b==0;
 	prime.isPrime = (n) => {
 		if(n<2) return prime.values.no;
-		var params = {'init': 2, 'len': (n/3)+1, 'usePrimes': false};
-		var pLen=prime.primes.length;
-		if(pLen>0) if(prime.primes[pLen-1]>params.len) params = {'init': 0, 'len': pLen, 'usePrimes': true};
-		for(var i=params.init; i<params.len; i++) {
-			if(!params.usePrimes) if(prime.bool(i)) prime.addPrime(i);
-			p = params.usePrimes?prime.primes[i]:i;
-			if (n!=p) if(prime.isFactor(n,p)) return prime.values.pf;
+		if(prime.primes.indexOf(n)>-1) return prime.values.yes;
+		for(var i=2; i<Math.floor(n/i)+1; i++) {
+			if(prime.bool(i)) {
+				prime.addPrime(i);
+				if(prime.isFactor(n,i)) return prime.values.pf;
+			}
 		}
 		prime.addPrime(n);
 		return prime.values.yes;
 	};
 	prime.factorize = (num, factors) => {
 		if(num<2) return factors;
-		var step = (n, next) => {
-			factors[n] |= 0;
-			factors[n]++;
-			return next();
-		};
-		if(prime.bool(num)) return step(num, () => factors);
-		for(var i=0; i<prime.primes.length; i++) {
-			if(prime.isFactor(num,prime.primes[i])) {
-				return step(prime.primes[i], () => prime.factorize(num/prime.primes[i], factors));
+		if(prime.bool(num)) {
+			factors[num] |= 0;
+			factors[num]++;
+			return factors;
+		}
+		var remain = num*1;
+		var p = (i) => prime.primes[i];
+		for (var i=0; i<Math.floor(remain/p(i)) || remain > 1; i++) {
+			if(prime.isFactor(remain,p(i))) factors[p(i)] |= 0;
+			while (prime.isFactor(remain,p(i)) && remain > 1) {
+				factors[p(i)]++;
+				remain /= p(i);
 			}
 		}
 		return factors;
